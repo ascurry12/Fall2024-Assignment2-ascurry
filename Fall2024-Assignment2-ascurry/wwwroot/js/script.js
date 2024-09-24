432c360f8b5295ad49086fb571f4554e4f452694
Aniyah C. <aniyahcurry12@gmail.com>
2024-09-23 20:58:58 -0500

    website

--- Fall2024-Assignment2-ascurry/wwwroot/js/script.js
+++ Fall2024-Assignment2-ascurry/wwwroot/js/script.js
@@ -1,29 +1,88 @@
-function apiSearch() {
+/*
+
+API key 1: 9dd006c27d3f4e0fbcb7d48a7b567d13
+endpoint url: https://api.bing.microsoft.com/v7.0/search?
+
+*/
+
+var background = true;
+var search = false;
+var lucky = false;
+
+function searchButtonClick() {
+    search = true;
+    apiSearch(search, lucky);
+    search = false;
+}
+
+function luckyButtonClick() {
+    lucky = true;
+    apiSearch(search, lucky);
+    lucky = false;
+}
+
+function timeButtonClick() {
+    const d = new Date;
+    var formmated = d.toLocaleString([], {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+
+    var time = `<p><span class="ui-icon ui-icon-clock" style="float:left; margin:0 7px 50px 0;"></span>${formmated}</p>`;
+    $('#time').html(time);
+    $('#time').dialog({ height: 90 });
+    $('#time').css('visibility', 'visible');
+}
+
+function theLostCity() {
+    window.open("https://en.wikipedia.org/wiki/Atlantis:_The_Lost_Empire");
+}
+
+function changeBackground() {
+    if (!background) {
+        $("#searchBody").css("background-image", "url(\"./images/jellyfish.jpg\")");
+    }
+    else {
+        $("#searchBody").css("background-image", "url(\"./images/underwater.jpg\")");
+    }
+
+    background = !background;
+}
+
+function apiSearch(search, lucky) {
     var params = {
         'q': $('#query').val(),
-        'count': 50,
+        'count': 10,
         'offset': 0,
         'mkt': 'en-us'
     };
 
     $.ajax({
-        url: 'https://MY-API-ENDPOINT/v7.0/search?' + $.param(params),
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
         type: 'GET',
         headers: {
-            'Ocp-Apim-Subscription-Key': 'MY-API-KEY'
+            'Ocp-Apim-Subscription-Key': '9dd006c27d3f4e0fbcb7d48a7b567d13'
         }
     })
         .done(function (data) {
-            var len = data.webPages.value.length;
-            var results = '';
-            for (i = 0; i < len; i++) {
-                results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a>: ${data.webPages.value[i].snippet}</p>`;
+            if (search) {
+                var len = data.webPages.value.length;
+                var results = '';
+                for (i = 0; i < len; i++) {
+                    results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a> <br> ${data.webPages.value[i].snippet}</p>`;
+                }
+
+                $('#searchResults').html(results);
+                $('#searchResults').css('visibility', 'visible');
             }
 
-            $('#searchResults').html(results);
-            $('#searchResults').dialog();
+            if (lucky) {
+                window.open(data.webPages.value[0].url);
+            }
+
         })
         .fail(function () {
             alert('error');
         });
 }
+
